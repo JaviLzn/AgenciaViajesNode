50ed47916cbda75de05b6435a5b16000b5321bef
Javier Lozano <ing.javierlozano@hotmail.com>
2020-07-27 21:31:21 -0500

    Se habilitar pug Se configura la ruta de las vistas

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 // importar express
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 // configurar express
 const app = express();
+
+// habilitar pug
+app.set('view engine', 'pug');
+
+
+// Añadir las vista
+app.set('views', path.join(__dirname, './views'));
+
+// cargar las rutas
 app.use('/', routes());
 
 
